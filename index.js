3ae00c34e2f141ea87b20fabe81b0c880d2dcb8a
pascaline <passyish5@gmail.com>
2019-07-16 10:49:14 +0200

    operators

---
+++ index.js
@@ -0,0 +1,12 @@
+ var favoriteDrink = "green tea";
+favoriteDrink;
+console.log(favoriteDrink);
+var favoriteNumber =42;
+favoriteNumber +=1;
+console.log(favoriteNumber);
+var myNumber = 3;
+console.log(myNumber < 5);
+console.log(myNumber >5);
+console.log(myNumber === 3);
+console.log(myNumber === 4);
+
