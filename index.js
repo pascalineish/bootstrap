ddc392f7a92cce2df4feecfce19f3bc041996fb0
pascaline <passyish5@gmail.com>
2019-07-16 11:08:25 +0200

    practice

--- index.js
+++ index.js
@@ -9,4 +9,12 @@ console.log(myNumber < 5);
 console.log(myNumber >5);
 console.log(myNumber === 3);
 console.log(myNumber === 4);
+var firstNumber =27;
+var secondNumber =42;
+console.log(firstNumber >secondNumber);
+console.log(firstNumber <secondNumber);
+console.log(firstNumber === secondNumber);
+console.log(firstNumber +=15); 
+console.log(secondNumber -=10);   
+
 
