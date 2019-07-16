9ab83bce7bc338ede088abef0f822b88a8902c41
pascaline <passyish5@gmail.com>
2019-07-16 16:18:51 +0200

    added function

--- index.js
+++ index.js
@@ -19,12 +19,22 @@ console.log(secondNumber -=10);
 var nn="67";
 var yy = 57;
 console.log(nn === yy); 
-var m= prompt("What is your favorite planet?") // I'm going to type "Earth"
-console.log(m);
-var x= alert("Hey there!");
-console.log(x);
-var u= prompt("Are you ready to code?").toUpperCase();
-console.log(u);
-var b=confirm("Would you like to continue?");
-console.log(b);
-
+//var m= prompt("What is your favorite planet?") // I'm going to type "Earth"
+//console.log(m);
+//var x= alert("Hey there!");
+//console.log(x);
+//var u= prompt("Are you ready to code?").toUpperCase();
+//console.log(u);
+//var b=confirm("Would you like to continue?");
+//console.log(b);
+//var age = prompt("Enter your age: ");
+//console .log( age+age);
+//var saySomething = function(whatToSay) {
+   // alert(whatToSay);
+//};
+//console .log(saySomething);
+//var add = function(number1, number2) {
+   // return number1 + number2;
+    //};
+//console.log(add);
+var add
