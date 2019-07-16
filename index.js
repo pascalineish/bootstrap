fa607bad5c1d5f63f78e8298f92a0ae9df20d343
pascaline <passyish5@gmail.com>
2019-07-16 11:47:46 +0200

    function

--- index.js
+++ index.js
@@ -15,6 +15,16 @@ console.log(firstNumber >secondNumber);
 console.log(firstNumber <secondNumber);
 console.log(firstNumber === secondNumber);
 console.log(firstNumber +=15); 
-console.log(secondNumber -=10);   
-
+console.log(secondNumber -=10);
+var nn="67";
+var yy = 57;
+console.log(nn === yy); 
+var m= prompt("What is your favorite planet?") // I'm going to type "Earth"
+console.log(m);
+var x= alert("Hey there!");
+console.log(x);
+var u= prompt("Are you ready to code?").toUpperCase();
+console.log(u);
+var b=confirm("Would you like to continue?");
+console.log(b);
 
